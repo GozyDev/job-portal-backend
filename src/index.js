138ccfbbfb8545370223d8e156cef7c745f47334
GozyDev <networkinggozy@gmail.com>
2026-03-16 07:06:22 +0100

    cors update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jobRoutes = require("./routes/jobRoutes");
 
 const app = express();
 
-app.use(cors());
+const allowedOrigin = process.env.FRONTEND_URL;
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+  }),
+);
 app.use(express.json());
 
 // Routes
